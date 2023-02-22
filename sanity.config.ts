262969aca5875ae06fb2fce5abb7bb457eda2d2f
James Trenda <jamestrenda@gmail.com>
2023-02-22 15:44:15 -0500

    add missing ! to silence typescript

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,8 @@ import { deskTool } from 'sanity/desk';
 
 // import { schemaTypes } from './schemas';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!!;
 
 export default defineConfig({
   basePath: '/admin', // <-- important that `basePath` matches the route you're mounting your studio from, it applies to both `/pages` and `/app`
